6ce82d14e4bcbb6cef0cbcc90fe965be229b00db
quyenhua <huathitoquyen0403@gmail.com>
2017-11-05 23:18:38 +0700

    Commit search song

---
+++ app/src/main/java/com/example/tiennguyen/thesis/activities/PlayingMusic.java
@@ -0,0 +1,15 @@
+package com.example.tiennguyen.thesis.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.tiennguyen.thesis.R;
+
+public class PlayingMusic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playing_music);
+    }
+}
