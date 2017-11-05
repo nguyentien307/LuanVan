f877104240058b6f4d29b964e95440452ce511a6
quyenhua <huathitoquyen0403@gmail.com>
2017-11-05 23:34:35 +0700

    Commit playing music UI

--- app/src/main/java/com/example/tiennguyen/thesis/activities/PlayingMusic.java
+++ app/src/main/java/com/example/tiennguyen/thesis/activities/PlayingMusic.java
@@ -2,14 +2,52 @@ package com.example.tiennguyen.thesis.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.tiennguyen.thesis.R;
 
-public class PlayingMusic extends AppCompatActivity {
+import co.mobiwise.library.InteractivePlayerView;
+import co.mobiwise.library.OnActionClickedListener;
+
+public class PlayingMusic extends AppCompatActivity implements OnActionClickedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playing_music);
+
+        final InteractivePlayerView mInteractivePlayerView = (InteractivePlayerView) findViewById(R.id.interactivePlayerView);
+        mInteractivePlayerView.setMax(114);
+        mInteractivePlayerView.setProgress(50);
+        mInteractivePlayerView.setOnActionClickedListener(this);
+
+        final ImageView imageView = (ImageView) findViewById(R.id.control);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mInteractivePlayerView.isPlaying()) {
+                    mInteractivePlayerView.start();
+                    imageView.setBackgroundResource(R.drawable.ic_action_pause);
+                } else {
+                    mInteractivePlayerView.stop();
+                    imageView.setBackgroundResource(R.drawable.ic_action_play);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onActionClicked(int id) {
+        switch (id) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
     }
 }
